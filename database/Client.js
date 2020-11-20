1cf7f41abcb92695711a5345e653e8f081489650
Iulian Novenschi <72090285+iuliannovenschi7@users.noreply.github.com>
2020-11-21 00:09:53 +0200

    Update Client.js

--- database/Client.js
+++ database/Client.js
@@ -2,6 +2,9 @@ const { Client, Collection } = require("discord.js");
 
 module.exports = class extends Client {
     constructor(config) {
+        super({
+            disableMentions: 'everyone'
+        });
 
         this.commands = new Collection();
 
