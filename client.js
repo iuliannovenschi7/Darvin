dbbf50221ab8eea0c1cf0db3eb3ec906621919d9
Iulian Novenschi <72090285+iuliannovenschi7@users.noreply.github.com>
2020-11-20 21:24:51 +0200

    installed node and created client.js

---
+++ client.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+client.login('Nzc5NDI2MDU4NTgzNDA4NjYx.X7gXFQ.wfFbMWs-sXvW_e_fvkVl19fdeqA');
