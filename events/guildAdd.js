63aeff1f452073eac482b5f6ce0d47aaee60a8b2
Iulian Novenschi <72090285+iuliannovenschi7@users.noreply.github.com>
2020-11-21 22:35:55 +0200

    Update guildAdd.js

--- events/guildAdd.js
+++ events/guildAdd.js
@@ -1,16 +1,8 @@
 const Discord = require('discord.js')
-module.exports = (bot, guild) => {
+module.exports =async(guild)=> {
     console.log(
-        `New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`
+        `New guild joined: ${guild.name} (id: ${guild.id}).
+         This guild has ${guild.memberCount} members!
+         Owner ${guild.owner.user.tag}`
     );
-    var logm = bot.channels.cache.get("779775995905310750");
-    const serverslog = new Discord.MessageEmbed()
-        .setTitle("New server :white_check_mark:")
-        .setColor('GREEN')
-        .addField("Name", `${guild.name}`, true)
-        .addField("Has:", `${guild.memberCount} members`, true)
-        .addField("owner:", `${guild.owner.user.tag}`, true)
-        .setTimestamp()
-        .setFooter(`ID: ${guild.id}`);
-    logm.send(serverslog)
 }
