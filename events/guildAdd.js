d3ecd4275e4e7b31417bf20f6fa951ce188e738c
Iulian Novenschi <72090285+iuliannovenschi7@users.noreply.github.com>
2020-11-21 22:06:17 +0200

    Create guildAdd.js

---
+++ events/guildAdd.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js')
+module.exports = (bot, guild) => {
+    console.log(
+        `New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`
+    );
+    var logm = bot.channels.get("779775995905310750");
+    const serverslog = new Discord.MessageEmbed()
+        .setTitle("New server :white_check_mark:")
+        .setColor('GREEN')
+        .addField("Name", `${guild.name}`, true)
+        .addField("Has:", `${guild.memberCount} members`, true)
+        .addField("owner:", `${guild.owner.user.tag}`, true)
+        .setTimestamp()
+        .setFooter(`ID: ${guild.id}`);
+    logm.send(serverslog)
+}
