a8e61dc84af2bb3ff7ec021020c883da474e0c85
Iulian Novenschi <72090285+iuliannovenschi7@users.noreply.github.com>
2020-11-21 22:08:44 +0200

    u

--- events/guildAdd.js
+++ events/guildAdd.js
@@ -3,7 +3,7 @@ module.exports = (bot, guild) => {
     console.log(
         `New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`
     );
-    var logm = bot.channels.get("779775995905310750");
+    var logm = bot.channels.cache.get("779775995905310750");
     const serverslog = new Discord.MessageEmbed()
         .setTitle("New server :white_check_mark:")
         .setColor('GREEN')
