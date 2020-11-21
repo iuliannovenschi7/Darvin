de56cfac885b80a3f4445317347282cc2c46d1d7
Iulian Novenschi <72090285+iuliannovenschi7@users.noreply.github.com>
2020-11-21 21:55:55 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,6 +3,4 @@ module.exports = (bot, ready) => {
     bot.user.setStatus("online");
     bot.user.setActivity(`Wild Rift`)
     console.log(bot.user.username + " is online and ready");
-    var logm = bot.channels.get("627162373156896778");
-    logm.send("online");
 }
